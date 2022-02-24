6a0243846bd5acfd4d4e6de3537685653f1e4af7
Caroline Nielsen <caro112f@stud.kea.dk>
2022-02-24 12:02:29 +0100

    cleaning works + images

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const Student = {
   firstName: "",
   lastName: "",
-  middleName: "",
+  middleName: undefined,
   nickName: "",
   gender: "",
   profilePic: "",
@@ -31,24 +31,131 @@ async function loadJSON() {
   hogwartsData = await jsonData.json();
   //show hogwarts data in a table in the console
   console.table(hogwartsData);
-  //call createStudents function
-  prepareStudents();
+  //call prepareStudents function
+  prepareStudents(hogwartsData);
 }
 function prepareStudents() {
-  firstName = getFirstName();
-  middleName = getMiddleName();
-  nickName = getNickName();
-  lastName = getLastName();
-  gender = getGender();
-  house = getHouse();
-  profilePic = getProfilePic();
+  hogwartsData.forEach((stud) => {
+    const student = Object.create(Student);
+    student.firstName = getFirstName(stud.fullname.trim());
+    student.middleName = getMiddleName(stud.fullname.trim());
+    student.nickName = getNickName(stud.fullname.trim());
+    student.lastName = getLastName(stud.fullname.trim());
+    student.gender = getGender(stud.gender.trim());
+    student.house = getHouse(stud.house.trim());
+    student.profilePic = getProfilePic(stud.fullname.trim());
+    allStudents.push(student);
+  });
 }
 
-function getFirstName() {}
-function getMiddleName() {}
-function getNickName() {}
-function getLastName() {}
-function getGender() {}
-function getHouse() {}
-function getProfilePic() {}
-function cleanData() {}
+function getFirstName(fullname) {
+  //find first name
+  if (fullname.includes(" ")) {
+    let firstName = fullname.substring(
+      fullname.indexOf(0),
+      fullname.indexOf(" ")
+    );
+    const cleanedFirstName = cleanData(firstName);
+    return cleanedFirstName;
+  } else {
+    const cleanedFirstName = cleanData(fullname);
+    return cleanedFirstName;
+  }
+}
+
+function getMiddleName(fullname) {
+  const middleSpace = fullname.slice(
+    fullname.indexOf(" ") + 1,
+    fullname.lastIndexOf(" ")
+  );
+
+  if (!fullname.includes('"') && fullname.includes(" ")) {
+    let middleName = middleSpace;
+    const cleanedMiddleName = cleanData(middleName);
+    return cleanedMiddleName;
+  } else if (middleSpace === "") {
+    let middleName = null;
+    return middleName;
+  }
+}
+function getNickName(fullname) {
+  if (fullname.includes('"')) {
+    let nickName = fullname.substring(
+      fullname.indexOf('"') + 1,
+      fullname.lastIndexOf('"')
+    );
+    const cleanedNickName = cleanData(nickName);
+    return cleanedNickName;
+  }
+}
+function getLastName(fullname) {
+  const lastName = fullname.substring(fullname.lastIndexOf(" ") + 1);
+
+  if (lastName.includes("-")) {
+    //split middleName into an array
+    const lastNameArray = lastName.split("");
+
+    lastNameArray.forEach((element, index, array) => {
+      // if the element contains a "-" or space make the letter afterwards uppercase
+      if (element === "-" || element === " ") {
+        array[index + 1] = array[index + 1].toUpperCase();
+      }
+    });
+
+    //then join the array into a new string by concat all elements in the array
+    let result = lastNameArray.join("");
+    return result;
+  } else if (!fullname.includes(" ")) {
+    let lastName = null;
+    return lastName;
+  } else {
+    const cleanedLastName = cleanData(lastName);
+    return cleanedLastName;
+  }
+}
+function getGender(gender) {
+  const cleanGender = cleanData(gender);
+  return cleanGender;
+}
+function getHouse(house) {
+  const cleanHouse = cleanData(house);
+  return cleanHouse;
+}
+
+function getProfilePic(fullname) {
+  const lastNameLowerCase = fullname
+    .substring(fullname.lastIndexOf(" ") + 1)
+    .toLowerCase();
+  const firstName = fullname.substring(
+    fullname.indexOf(0),
+    fullname.indexOf(" ")
+  );
+  const firstNameLowerCase = firstName.toLowerCase();
+
+  const smallFirstLetterOfFirstName = firstNameLowerCase
+    .slice(0, 1)
+    .toLowerCase();
+
+  if (lastNameLowerCase === "patil") {
+    const profilePic = `images/${lastNameLowerCase}_${firstNameLowerCase}.png`;
+    return profilePic;
+  } else if (lastNameLowerCase.includes("-")) {
+    const secondLastNameLowerCase = lastNameLowerCase.slice(
+      lastNameLowerCase.indexOf("-") + 1
+    );
+    const profilePic = `images/${secondLastNameLowerCase}_${smallFirstLetterOfFirstName}.png`;
+    return profilePic;
+  } else {
+    const profilePic = `images/${lastNameLowerCase}_${smallFirstLetterOfFirstName}.png`;
+    return profilePic;
+  }
+}
+
+function cleanData(data) {
+  //trim + capitalize
+  const capitalizeFirstLetter = data.slice(0, 1).toUpperCase();
+  const lowercaseTheRest = data.slice(1).toLowerCase();
+  const cleanedData = capitalizeFirstLetter + lowercaseTheRest;
+
+  return cleanedData;
+}
